3fe556af107b4d8f66abeb587bd8816611fea5be
zhh <zhh@qq.com>
2018-09-15 18:05:17 +0800

    day01

---
+++ day01/flower.js
@@ -0,0 +1,43 @@
+var xiaoming = {
+  name: '小明',
+  sex: '男',
+  single: true,
+  classroom: '161',
+  hometown: '赣州',
+  sendFlower: function (target) {
+    console.log(this.name + '送花给' + target.name)
+    target.receiveFlower(this)
+  }
+}
+
+var xiaohong = {
+  name: '小红',
+  sex: '女',
+  single: true,
+  classroom: '161',
+  receiveFlower: function (sender) {
+    if(sender.name === '小丽'){
+      console.log('看个电影试试')
+    }else{
+      console.log('扔了')
+    }
+  },
+  listenGoodMod: function (people) {
+    setTimeout(() => {
+      this.receiveFlower(people)
+    }, 10000);
+  }
+}
+
+var xioali = {
+  name: '小丽',
+  sex: '女',
+  single: true,
+  hometown: '赣州',
+  receiveFlower: function (sender) {
+    xiaohong.listenGoodMod(this)
+  }
+}
+
+// xiaoming.sendFlower(xiaohong)
+xiaoming.sendFlower(xioali)
---
+++ day01/readme.md
@@ -0,0 +1,16 @@
+# Talk in js 之代理模式
+
+- 将JS的面向对象通过送花的情景讲出来
+  + 对象JSON Object，描述性，js是动态性灵活的语言 { }    Object
+  + 将现实世界跟代码结合，属性和方法，组成的复杂数据结构
+  key: value 值为function 方法，对象有行为或方法
+  + 接口，两个对象实现同样的方法，可以在执行中互换使用，这是代理模式的核心
+- 代理模式
+  + Proxyable, 实现了更复杂更有用的功能，更好的控制对象
+
+
+- js 语法
+  + JSON Object
+  + this 关键字 在对象内部使用this对象本身
+  + 定时器
+    setTimeout(fun, time)

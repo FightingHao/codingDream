76d238d60ddd720c3d370c2d79411292e61e5150
FightingHao <496915484@qq.com>
2018-09-28 23:27:45 +0800

    js类型

---
+++ day08/js/type_test/1.js
@@ -0,0 +1,28 @@
+var und = undefined
+var nul = null
+var boo = true
+var num = 1
+var str = 'hao'
+// Symbol
+
+// 复杂数据类型 引用类型
+var obj = { a: 1 }
+var func = function () {}
+var arr = [1, 2, 3]
+/* var obj1 = {}
+// Object.assign(obj1, obj) // 浅拷贝
+for (var key in obj) {
+  obj1[key] = obj[key]
+}
+obj1.a = 2
+console.log(obj1)
+console.log('----')
+console.log(obj) */
+console.log(typeof str); //string
+console.log(typeof num); //number
+console.log(typeof und); //undefined
+console.log(typeof boo); //boolean
+console.log(typeof nul); //object
+console.log(typeof func); //function
+console.log('');
+
---
+++ day08/js/type_test/date.js
@@ -0,0 +1,3 @@
+var myDate = new Date()
+
+console.log(myDate.getTime());

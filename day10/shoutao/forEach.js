be69447dc25b8795f3a57f1f025974eeec4357bd
FightingHao <496915484@qq.com>
2018-10-08 23:53:13 +0800

    JS语法篇——函数

---
+++ day10/shoutao/forEach.js
@@ -0,0 +1,20 @@
+// 手写forEach代码
+Array.prototype.forEach = function (fn) {
+  if (typeof fn != 'function') {
+    throw new Error('参数必须为函数')
+  }
+
+  if (!Array.isArray(this)) {
+    throw new Error('必须用数组调用该方法')
+  }
+  for (let index = 0; index < this.length; index++) {
+    // 执行回调函数
+    fn(arr[index], index, this)
+  }
+}
+
+
+const arr = new Array(1, 2, 3)
+arr.forEach(function (item, index, array) {
+  console.log(item, index, array)
+})

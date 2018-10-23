f855d6432dc2df6dc3e30e2c6de9414b8e65eeb6
FightingHao <496915484@qq.com>
2018-10-23 23:30:29 +0800

    小程序云开发

--- 小程序/cloud_weather/miniprogram/pages/index/index.js
+++ 小程序/cloud_weather/miniprogram/pages/index/index.js
@@ -1,8 +1,14 @@
+// 云能力初始化
 wx.cloud.init({
-  env: 'zhh-cloud-b7a1a9'
+  // 传入字符串形式的环境 ID 可以指定所有服务的默认环境
+  env: 'zhh-cloud-b7a1a9',
+  // 是否在将用户访问记录到用户管理中，在控制台中可见
+  traceUser: true
 })
 
-const db = wx.cloud.database()
+const db = wx.cloud.database({
+  env: 'zhh-cloud-b7a1a9'
+})
 const productsCollection = db.collection('products')
 const _ = db.command
 

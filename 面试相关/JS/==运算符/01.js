b189d6212376c054206f741f2598423bbf067f93
FightingHao <496915484@qq.com>
2019-01-15 11:08:17 +0800

    运算符相关

--- 面试相关/JS/==运算符/01.js
+++ 面试相关/JS/==运算符/01.js
@@ -3,5 +3,5 @@ console.log(Number === Number); // true
 console.log(null == undefined); //true
 console.log(null === undefined); //false
 console.log(NaN == NaN); // false
-console.log(NaN === NaN); // false 
+console.log(NaN === NaN); // false
 

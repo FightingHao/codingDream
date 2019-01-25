8f72c621726c89d5e59f4de7d3eaee01711d939f
FightingHao <496915484@qq.com>
2019-01-25 17:34:48 +0800

    defineProperty

---
+++ 面试相关/JS/Object类静态方法/04_defineProperty.js
@@ -0,0 +1,36 @@
+var o = {}; // 创建一个新对象
+
+// 在对象中添加一个属性与数据描述符的示例
+Object.defineProperty(o, "a", {
+  value: 37,
+  writable: true,
+  enumerable: true,
+  configurable: true
+});
+// 对象o拥有了属性a，值为37
+
+// 在对象中添加一个属性与存取描述符的示例
+var bValue;
+Object.defineProperty(o, "b", {
+  get: function () {
+    return bValue;
+  },
+  set: function (newValue) {
+    bValue = newValue;
+  },
+  enumerable: true,
+  configurable: true
+});
+o.b = 38;
+// 对象o拥有了属性b，值为38
+
+// o.b的值现在总是与bValue相同，除非重新定义o.b
+
+// 数据描述符和存取描述符不能混合使用
+Object.defineProperty(o, "conflict", {
+  value: 0x9f91102,
+  // set: function () {
+  //   // return 0xdeadbeef;
+  // }
+});
+// TypeError: Invalid property descriptor. Cannot both specify accessors and a value or writable attribute(无效的属性描述符,不能同时指定访问器和值或可写属性)

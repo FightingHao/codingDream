a0a53b08351914193cd1e3bfc6a066cc51e7b009
FightingHao <496915484@qq.com>
2019-03-18 11:13:37 +0800

    concat

---
+++ 面试相关/JS/数组常用方法/concat.js
@@ -0,0 +1,11 @@
+var arr1 = [1, 2]
+// 可一个参数一个参数的传递
+arr1.concat(3, 4)
+
+// 不改变原数组
+console.log(arr1) // [1, 2, 3, 4]
+
+var arr2 = [1, 2]
+// 可传递数组
+var arr3 = arr2.concat([3, 4])
+console.log(arr3) // [1, 2, 3, 4]

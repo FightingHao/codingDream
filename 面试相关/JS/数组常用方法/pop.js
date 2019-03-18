d38e92b98d9efb206cc6e573fcb3f45355e978c2
FightingHao <496915484@qq.com>
2019-03-18 11:13:57 +0800

    pop

---
+++ 面试相关/JS/数组常用方法/pop.js
@@ -0,0 +1,15 @@
+var arr1 = [1, 2, 4]
+var result = arr1.pop()
+
+console.log(result) // 4
+
+// 改变原数组
+console.log(arr1) // [1, 2]
+
+
+var arr2 = []
+result = arr2.pop()
+
+console.log(result) // 数组为空返回undefined
+
+console.log(arr2) // []

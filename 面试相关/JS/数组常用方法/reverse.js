b92906ebd8e71f20137596a81680515fb3af054a
FightingHao <496915484@qq.com>
2019-03-20 23:21:59 +0800

    reverse

---
+++ 面试相关/JS/数组常用方法/reverse.js
@@ -0,0 +1,13 @@
+var arr1 = [1, 2, 3]
+
+// 可传多个参数
+var result = arr1.reverse()
+
+// 返回颠倒后数组
+console.log(result) // [3, 2, 1]
+
+// 改变原数组
+console.log(arr1) // [3, 2, 1]
+
+// 返回值等于原数组
+console.log(arr1 === result) // true

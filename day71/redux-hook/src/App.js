1c01688f95e50efb9f5131d2b6ebec1624ed0049
邹豪豪 <496915484@qq.com>
2019-03-29 20:54:54 +0800

    react-hooks useReducer

---
+++ day71/redux-hook/src/App.js
@@ -0,0 +1,34 @@
+import React, { useReducer } from 'react'
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'reset':
+      return initialState;
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'reset' })}>
+        Reset
+      </button>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </div>
+  )
+
+}
+
+export default App

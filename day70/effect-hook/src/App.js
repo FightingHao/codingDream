2b8c2878e208672cf93e86116756c8f998c287a4
邹豪豪 <496915484@qq.com>
2019-04-08 10:19:52 +0800

    effect-hook

--- day70/effect-hook/src/App.js
+++ day70/effect-hook/src/App.js
@@ -1,18 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
-const App = (props) => {
+const App = () => {
   const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
 
-  // 相当于 componentDidMount 和 componentDidUpdate，每次render都会执行
+  const initData = async () => {
+    // 发起请求并执行初始化操作
+  }
+
+  const stop = async () => {
+    console.log('clear')
+  }
+
+  // 执行初始化操作，需要注意的是，如果你只是想在渲染的时候初始化一次数据，那么第二个参数必须传空数组。
   useEffect(() => {
-    document.title = `click ${count} count`
-    console.log(props)
-    
-  })
+    console.log('useEffect')
+    console.log(count2)
+    initData()
+    return stop
+  }, [count2])
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>click me</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>click</button>
+      <button onClick={() => setCount2(count2 + 1)}>click</button>
     </div>
   )
 }

ef295c985db346bb824c58781c6757fa2cd7905b
FightingHao <496915484@qq.com>
2019-07-24 12:49:10 +0800

    数组扁平化

--- 面试相关/JS/笔试题/数组扁平化/reduce.js
+++
@@ -1,8 +0,0 @@
-const arr = [1, [2, 3], [4, [5]]]
-
-function flatten(arr) {
-  return arr.reduce((pre, next) => pre.concat(Array.isArray(next) ? flatten(next) : next), [])
-}
-
-const res = flatten(arr)
-console.log(res)

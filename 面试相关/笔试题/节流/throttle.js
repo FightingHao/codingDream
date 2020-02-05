af3489bb911cbb9396e30753de77e882c0d5dc1f
FightingHao <40445145+FightingHao@users.noreply.github.com>
2020-02-05 10:16:39 +0800

    Update throttle.js

--- 面试相关/笔试题/节流/throttle.js
+++ 面试相关/笔试题/节流/throttle.js
@@ -1,12 +1,24 @@
+// 时间戳版本
 function throttle(fn, time) {
-  var lastTime = 0
-
+  let lastTime = 0;
   return function () {
-    var nowTime = Date.now()
-
+    let nowTime = Date.now();
     if (nowTime - lastTime > time) {
-      fn()
-      lastTime = nowTime
+      fn.call(this, arguments);
+      lastTime = nowTime;
+    }
+  }
+}
+
+// 定时器版本
+function throttle(fn, time) {
+  let timeout = null;
+  return function () {
+    if (!timeout) {
+      timeout = setTimeout(_ => {
+        fn.call(this, arguments);
+        timeout = null;
+      }, time)
     }
   }
 }
